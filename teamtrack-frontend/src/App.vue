6188b8a8473b16e220fb33e4816145eee3162434
Opilane TTHK <opilane@tthk.ee>
2024-02-16 10:48:22 +0200

    committt

---
+++ teamtrack-frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+        <nav>
+            <ul>
+                <li><a href="http://localhost:5173/">Home Page</a></li>
+                <li><a href="/coachEvents">Coach</a></li>
+                <li><a href="/athleteEvents">Athlete</a></li>
+            </ul>
+        </nav>
+
+        <router-view />
+</template>
+
+<script setup lang="ts">
+    import {defineProps} from "vue";
+    defineProps<{ isAthlete: Boolean, hasChosen: Boolean}>();
+
+</script>
+
+
+
+
