6a7845d57d6f4fa76c18df814c931d34513a662d
Opilane TTHK <opilane@tthk.ee>
2024-02-22 13:12:28 +0200

    Navbar

--- teamtrack-frontend/src/App.vue
+++ teamtrack-frontend/src/App.vue
@@ -1,20 +1,48 @@
 <template>
-        <nav>
-            <ul>
-                <li><a href="http://localhost:5173/">Home Page</a></li>
-                <li><a href="/coachEvents">Coach</a></li>
-                <li><a href="/athleteEvents">Athlete</a></li>
-            </ul>
-        </nav>
-
-        <router-view />
+    
+      <Navbar />
+    <router-view />
 </template>
 
+
+
 <script setup lang="ts">
+    import Navbar from "./components/Navbar.vue";
     import {defineProps} from "vue";
     defineProps<{ isAthlete: Boolean, hasChosen: Boolean}>();
-
 </script>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  border: 1px solid #e7e7e7;
+  background-color: #f3f3f3;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: #666;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #ddd;
+}
+
+li a.active {
+  color: white;
+  background-color: #04AA6D;
+}
+</style>
 
 
 
