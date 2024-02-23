59823e4bf2d65c1dee0b3d2174d86ceeb7e16491
Opilane TTHK <opilane@tthk.ee>
2024-02-23 10:14:52 +0200

    brokenNavBar

--- teamtrack-frontend/src/App.vue
+++ teamtrack-frontend/src/App.vue
@@ -1,16 +1,58 @@
 <template>
-    
-      <Navbar />
-    <router-view />
+  <Column v-if="check">
+    <Navbar /> 
+  </Column>
+<router-view />
 </template>
 
 
 
 <script setup lang="ts">
     import Navbar from "./components/Navbar.vue";
-    import {defineProps} from "vue";
+    import {defineProps, watch} from "vue";
+    import { useRoute } from "vue-router";
     defineProps<{ isAthlete: Boolean, hasChosen: Boolean}>();
+
+    const route = useRoute();
+    var check = Boolean();
+
+    watch(route); {
+  if (route.path = "/"){
+    check = true;
+  }}
 </script>
+
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  border: 1px solid #e7e7e7;
+  background-color: #f3f3f3;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: #666;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #ddd;
+}
+
+li a.active {
+  color: white;
+  background-color: #04AA6D;
+}
+</style>
 
 <style>
 ul {
