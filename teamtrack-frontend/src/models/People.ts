6b45912abec7676b6d4488c55027a92b4bc3be8a
Opilane TTHK <opilane@tthk.ee>
2024-03-07 12:19:09 +0200

    Controller

---
+++ teamtrack-frontend/src/models/People.ts
@@ -0,0 +1,7 @@
+export interface Event {
+    Id: Number;
+    username: String;
+    email: String;
+  }
+
+  
---
+++ teamtrack-frontend/src/models/eventPeople.ts
@@ -0,0 +1,6 @@
+export interface Event {
+    Id: Number;
+    EventId: String;
+    PersonId: String;
+  }
+  
